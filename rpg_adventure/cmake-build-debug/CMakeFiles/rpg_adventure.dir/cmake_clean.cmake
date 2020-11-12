a0822e455d8cf49f2d009ae145a6e5be6711ed73
Diego <diegofp95@gmail.com>
2020-11-12 15:30:38 +0100

    created project in CLion

---
+++ rpg_adventure/cmake-build-debug/CMakeFiles/rpg_adventure.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rpg_adventure.dir/main.cpp.o"
+  "rpg_adventure"
+  "rpg_adventure.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rpg_adventure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ rpg_adventure/cmake-build-debug/CMakeFiles/rpg_adventure.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for rpg_adventure.
+# This may be replaced when dependencies are built.
