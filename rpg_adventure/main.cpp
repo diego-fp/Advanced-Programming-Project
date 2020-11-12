8e732dcf105a92b7270eeaca514285adb2754460
Diego <diegofp95@gmail.com>
2020-11-12 15:36:36 +0100

    Added text in initial menu

--- rpg_adventure/main.cpp
+++ rpg_adventure/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Welcome to the RPG adventure!\n Please select one option:" << std::endl;
+    std::cout << "1.- New Game" << std::endl;
+    std::cout << "2.- Load Game" << std::endl;
+    std::cout << "1.- Exit" << std::endl;
     return 0;
 }
