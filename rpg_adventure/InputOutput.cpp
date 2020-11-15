725222c2fd4b3064c401969cf0f6980e3b684233
Anna Prado <anna.prado@tum.de>
2020-11-15 11:21:28 +0100

    Add a WriteState function

--- rpg_adventure/InputOutput.cpp
+++ rpg_adventure/InputOutput.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "InputOutput.h"
+#include <fstream>
+using namespace std;
+
+int InputOutput::WriteState(Player player, string FileName) {
+    ofstream outfile(FileName);
+    if (! outfile.is_open()) {return EXIT_FAILURE; }
+    outfile << player.name() << " " << player.HP() << endl;
+    return 0;
+}
+
+string InputOutput::ReadState(std::string FileName) {}
--- rpg_adventure/InputOutput.h
+++ rpg_adventure/InputOutput.h
@@ -4,9 +4,14 @@
 
 #ifndef RPG_ADVENTURE_INPUTOUTPUT_H
 #define RPG_ADVENTURE_INPUTOUTPUT_H
-
+#include "Player.h"
+#include <iostream>
 
 class InputOutput {
+public:
+    // InputOutput();
+    int WriteState(Player player, std::string FileName);
+    std::string ReadState(std::string FileName);
 
 };
 
